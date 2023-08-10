ae4877da730887ba185217b880963badd1753aa9
Dalton, Rebecca Lynne <rebedalt@iu.edu>
2023-08-10 16:30:40 -0400

    redid addition
    - Added a dedicated function to retrieve the values of the matrix
    - redid addMatrix function to just add the matrices once they have been retreived using the prior function
    - Added 2D arrays matrix and matrix2 which hold the matrices.
    - Removed x and y in favor of x1, y1, x2 and y2. This will support matrices which different sizes (for operations which support them)

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const addButton = document.getElementById("add");
 const errorDiv = document.getElementById('error');
 const sumTable = document.getElementById('sum');
 
+let matrix1 = [[], []]; 
+let matrix2 = [[], []];
 let x = 0, y = 0; 
+let x2 = 0, y2 = 0;  
 function clearScreen() {
     errorDiv.innerHTML = "";
     f1.innerHTML = "";
     f2.innerHTML = "";
     addButton.style.visibility = "hidden";
     sumTable.innerHTML += "";
+}
+function clearArrays() {
+    matrix1 = [[], []]; 
+    matrix2 = [[], []];
 }
 function drawMatrix(num, x, y) {
     "use strict";
@@ -44,30 +51,59 @@ function drawBoxesAddition() {
         addButton.style.visibility = "visible";
     }
 }
-function addMatrices() { 
-    "use strict"; 
-    event.preventDefault();
-    for (let i = 1; i <= y; i++) {
-        var row = sumTable.insertRow(); 
-        for(let j = 1; j <= x; j++) {
-            let name = "m1r" + String(j) + "c" + String(i); 
-            if(document.getElementById(name)) {
-                let cell1 = document.getElementById(name), cell2 = document.getElementById("m2r" + String(j) + "c" + String(i));
-                let value1 = parseFloat(cell1.value, 10), value2 = parseFloat(cell2.value, 10); 
-                if(isNaN(value1) || isNaN(value2)) {
+function assignArrays(w1, h1, w2, h2) {
+    matrix1 = [[w1], [h1]];
+    matrix2 = [[w2], [h2]]; 
+    for(let i = 0; i < h1; i++) {
+        for(let j = 0; j < w1; j++) {
+            let name = "m1r" + String(j + 1) + "c" + String(i + 1); 
+            if (document.getElementById(name)) {
+                let cell = document.getElementById(name); 
+                let value = cell.value; 
+                if(isNaN(value)) {
                     clearScreen(); 
                     errorDiv.innerHTML = "Invalid Input";
                 }
                 else {
-                    var cell = row.insertCell();
-                    cell.innerHTML = String(value1 + value2);  
+                    matrix1[j][i] = parseFloat(value); 
                 }
             }
             else {
                 clearScreen(); 
-                errorDiv.innerHTML = "Unknown Error occurred <br/>"; 
-                errorDiv.innerHTML += "x = " + String(j) + ", y = " + String(i);
+                errorDiv.innerHTML = "Unknown error occurred";             }
+        }
+    }
+    for(let i = 0; i < h2; i++) {
+        for(let j = 0; j < w2; j++) {
+            let name = "m2r" + String(j + 1) + "c" + String(i + 1); 
+            if (document.getElementById(name)) {
+                let cell = document.getElementById(name); 
+                let value = cell.value; 
+                if(isNaN(value)) {
+                    clearScreen(); 
+                    errorDiv.innerHTML = "Invalid Input";
+                }
+                else {
+                    matrix2[j][i] = parseFloat(value); 
+                }
             }
+            else {
+                clearScreen(); 
+                errorDiv.innerHTML = "Unknown Error Occurred";
+            }
+        }
+    }
+}
+function addMatrices() { 
+    "use strict"; 
+    event.preventDefault();
+    assignArrays(x, y, x, y);
+    clearScreen(); 
+    for(let i = 0; i < y; i++) {
+        let row = sumTable.insertRow(); 
+        for(let j = 0; j < x; j++) {
+            let cell = row.insertCell(); 
+            cell.innerHTML = String(matrix1[j][i] + matrix2[j][i]); 
         }
     }
 }
